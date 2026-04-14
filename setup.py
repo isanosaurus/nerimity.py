0a87fdaf8c46d6297b7b46c4e43fa5ff3e843f1a
Jason <81298350+isanosaurus@users.noreply.github.com>
2026-04-14 23:05:53 +0200

    update to new release version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ readme = (readme_path / "README.md").read_text()
 
 setup (
     name="nerimity",
-    version="1.5.1",
+    version="1.5.2",
     packages=find_packages(),
     long_description=readme,
     long_description_content_type="text/markdown",
